d62379447d22c85c97188e13b99124380f11c83a
zIswim <enphkosta@naver.com>
2024-10-01 03:56:02 +0900

    removed cached

--- Node-React/config/key.js
+++
@@ -1,6 +0,0 @@
-//환경 변수가 local 환경에서는 development 모드, 배포 : production
-if(process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
-} else{
-    module.exports = require('./dev');
-}
